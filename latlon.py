967b259126b4ec4c44b8fa58dbf8c36fa43f4e85
Dylan Vasey <dyvasey@gmail.com>
2021-02-08 10:36:07 -0800

    Cleanup of latlon

--- latlon.py
+++ latlon.py
@@ -1,12 +1,22 @@
-# -*- coding: utf-8 -*-
 """
-Created on Thu Nov 19 11:39:37 2020
-
-@author: dyvas
+Module for basic transformations involving latitude/longitude
 """
-#Lat/Long decimal degrees from DMS calculator
-
 def dms2deg(latdeg,londeg,latmin,lonmin,latsec=0,lonsec=0):
+    """
+    Calculate latitude/longitude in decimal degrees from dms values.
+    
+    Parameters:
+        latdeg: Latitude degrees
+        londeg: Longitude degrees
+        latmin: Latitude minutes
+        lonmin: Longitude minutes
+        latsec: Latitude seconds
+        lonsec: Longitude seconds
+    
+    Returns:
+        lat: Latitude in decimal degrees
+        lon: Longitude in decimal degrees
+    """
     lat = (latdeg + (latmin+latsec/60)/60)
     lon = (londeg + (lonmin+lonsec/60)/60)
     return(lat,lon)
