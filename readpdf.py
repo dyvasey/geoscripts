61725d31e328bd42ee57e04c95612cecb40644b4
Dylan Vasey <dyvasey@gmail.com>
2021-02-08 10:48:28 -0800

    Cleanup of articleocr

--- readpdf.py
+++ readpdf.py
@@ -36,33 +36,45 @@ def article(pdf,page,filename,**kwargs):
     return(df)
 
 def articleocr(pdf,page):
-    #function to extract table from PDF article with no prior OCR
-    #Converts page to image, then writes OCR with tesseract, then 
-    #extracts table with tabula. Likely requires additional cleanup
+    """
+    Extract table from PDF with no embedded text using OCR and tabula.
     
-    # Add Tesseract executable to path, needed for OCR
+    Function uses Tesseract to implement optical character recognition (OCR),
+    then extracts table with tabula. Requires Tesseract to be installed 
+    locally with executable path defined in function. Resulting tables likely
+    to have errors and need additional cleanup.
+    
+    Parameters:
+        pdf: Name of PDF file in folder defined within function
+        page: Page of PDF with table
+        filename: Name of .csv table to output
+    
+    Returns:
+        df: Pandas dataframe with table
+    """
+    # Add Tesseract executable to path, needed for OCR.
     # Requires IDE in admin mode
     tpath = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
     pytesseract.pytesseract.tesseract_cmd = tpath
     
+    # Must set to path with PDFs on local machine
     path = "C:/Users/dyvas/Box Sync/JournalArticles/" + pdf + ".pdf"
+    
+    # Convert PDF page to image
     image = convert_from_path(path,first_page=page,last_page=page)
-    #image[0].save(pdf + '.jpg','JPEG')
 
     # Get a searchable PDF
     file = pytesseract.image_to_pdf_or_hocr(image[0], extension='pdf')
     newpath = pdf + '_' + str(page) + '_ocr.pdf'
     with open(newpath, 'w+b') as f:
-        f.write(file) # pdf type is bytes by default
+        f.write(file) # PDF type is bytes by default
         
     #Extract table with tabula
-
     dfs = tabula.read_pdf('./'+ newpath,pages=1)
+    df = dfs[0] # Convert to single dataframe
     
-    df = dfs[0] #convert to single dataframe
-    
+    # Save .csv with extracted table
     outdir = './' + pdf + '_' + str(page) + '.csv'
-    
     df.to_csv(outdir,index=False)
     
     return(df)
