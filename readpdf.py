d8d555506d36d2ad7141c4ee1b3144935977dfb8
Dylan Vasey <dyvasey@gmail.com>
2021-03-01 12:35:14 -0800

    Added image ocr to readpdf.

--- readpdf.py
+++ readpdf.py
@@ -47,7 +47,6 @@ def articleocr(pdf,page):
     Parameters:
         pdf: Name of PDF file in folder defined within function
         page: Page of PDF with table
-        filename: Name of .csv table to output
     
     Returns:
         df: Pandas dataframe with table
@@ -75,6 +74,45 @@ def articleocr(pdf,page):
     
     # Save .csv with extracted table
     outdir = './' + pdf + '_' + str(page) + '.csv'
+    df.to_csv(outdir,index=False)
+    
+    return(df)
+
+def imgocr(img):
+    """
+    Extract table from JPG image using OCR and tabula.
+    
+    Function uses Tesseract to implement optical character recognition (OCR),
+    then extracts table with tabula. Requires Tesseract to be installed 
+    locally with executable path defined in function. Resulting tables likely
+    to have errors and need additional cleanup.
+    
+    Parameters:
+        img: Name of image file in current directory
+    
+    Returns:
+        df: Pandas dataframe with table
+    """
+    # Add Tesseract executable to path, needed for OCR.
+    # Requires IDE in admin mode
+    tpath = 'C:/Program Files/Tesseract-OCR/tesseract.exe'
+    pytesseract.pytesseract.tesseract_cmd = tpath
+    
+    # Open the image
+    image = Image.open(img)
+
+    # Get a searchable PDF
+    file = pytesseract.image_to_pdf_or_hocr(image, extension='pdf')
+    newpath = img[0:-4] + '_ocr.pdf'
+    with open(newpath, 'w+b') as f:
+        f.write(file) # PDF type is bytes by default
+        
+    #Extract table with tabula
+    dfs = tabula.read_pdf('./'+ newpath,pages=1)
+    df = dfs[0] # Convert to single dataframe
+    
+    # Save .csv with extracted table
+    outdir = './' + img[0:-4] + '.csv'
     df.to_csv(outdir,index=False)
     
     return(df)
