e9113140b6bba96abce4e205bf3b61f6b344d284
Dylan Vasey <dyvasey@gmail.com>
2021-03-05 17:14:20 -0800

    Added crude KDE plotting to dz

--- dz.py
+++ dz.py
@@ -1,8 +1,16 @@
 """
 Module for processing and plotting detrital zircon data
 """
+import seaborn as sns
+
 class DZSample:
     """ Object to hold detrital zircon sample metadata and ages """
-    def __init__(self,location,agedata):
+    
+    # Define basic attributes
+    def __init__(self,name,location=None,agedata=None):
+        self.name = name
         self.location = location
         self.agedata = agedata
+
+    def kde(self,**kwargs):
+        sns.kdeplot(self.agedata['238 Age'],log_scale=True,**kwargs)
