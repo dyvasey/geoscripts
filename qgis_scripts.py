3a2efe44b44268ab22bbbcb5a2379ab29470579a
Dylan Vasey <dyvasey@gmail.com>
2021-03-12 17:06:16 -0800

    Started QGIS scripts. Wrote script to export color symbology into csv.

---
+++ qgis_scripts.py
@@ -0,0 +1,38 @@
+"""
+Scripts for use in QGIS
+
+Functions need to be run from QGIS console as currently written
+"""
+import pandas as pd
+import os
+
+def get_colors(filename='colors.csv'):
+    """
+    Get colors for active layer in QGIS
+    """
+    # Get active layer
+    layer = iface.activeLayer()
+
+    # Get to the symbology
+    renderer = layer.renderer()
+    categories = renderer.categories()
+
+    # Get name and corresponding color
+    names = []
+    colors = []
+    for cat in categories:
+        properties = cat.symbol().symbolLayer(0).properties()
+        colors.append(properties['color'])
+        names.append(cat.value())
+    
+    # Put into a Pandas series
+    output = pd.Series(data=colors,index=names)
+    
+    # Get working path for QGIS project
+    working_path = QgsProject.instance().readPath("./")
+    csv_path = os.path.join(working_path,filename)
+    
+    # Write CSV
+    output.to_csv(csv_path)
+
+    return(names,colors)
