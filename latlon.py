4dae7bd583ea8bcdb6fc5600be02d97e7da3c070
Dylan Vasey <dyvasey@gmail.com>
2021-03-12 17:07:52 -0800

    Added UTM to latlon conversion to latlon.py.

--- latlon.py
+++ latlon.py
@@ -1,6 +1,8 @@
 """
 Module for basic transformations involving latitude/longitude
 """
+import cartopy.crs as ccrs
+
 def dms2deg(latdeg,londeg,latmin,lonmin,latsec=0,lonsec=0):
     """
     Calculate latitude/longitude in decimal degrees from dms values.
@@ -19,4 +21,18 @@ def dms2deg(latdeg,londeg,latmin,lonmin,latsec=0,lonsec=0):
     """
     lat = (latdeg + (latmin+latsec/60)/60)
     lon = (londeg + (lonmin+lonsec/60)/60)
+    return(lat,lon)
+
+def UTM2latlon(easting,northing,zone,south=False):
+    """
+    Calculate latitude/longitude from UTM.
+    """
+    # Set ouptput to lat/lon
+    crs = ccrs.Geodetic()
+    
+    # Convert
+    lon,lat = crs.transform_point(
+        easting,northing,src_crs=ccrs.UTM(zone=zone,
+                                          southern_hemisphere=south))
+    
     return(lat,lon)
